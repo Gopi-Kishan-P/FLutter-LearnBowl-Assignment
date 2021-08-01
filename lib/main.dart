0808f2fc954d8053b0cedaae62b72d219373d8d6
Gopi Kishan P <gopi1792000@gmail.com>
2021-08-01 13:13:12 +0530

    Add HomePage, App Icon and Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:class_schedule/models/data.dart';
+import 'package:class_schedule/widgets/schedule.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -13,7 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(CustomColors.primaryMaterialColor,
+            CustomColors.primaryMaterialColorSwatch),
         scaffoldBackgroundColor: Colors.white,
       ),
       home: MyHomePage(),
@@ -24,21 +27,72 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<ClassSchedule> _classSchedule = [
+      ClassSchedule(
+        subjectName: 'Alphabets',
+        startTime: '09:00 am',
+        endTime: '09:50 am',
+        instructor: 'Mr. Rajesh',
+      ),
+      ClassSchedule(
+        subjectName: 'Greetings',
+        startTime: '10:00 am',
+        endTime: '10:50 am',
+        instructor: 'Mr. Kumar',
+      ),
+      ClassSchedule(
+        subjectName: 'Practise with Communication',
+        startTime: '11:00 am',
+        endTime: '11:50 am',
+        instructor: 'Mrs. Nandini',
+      ),
+      ClassSchedule(
+        subjectName: 'Accents',
+        startTime: '12:00 pm',
+        endTime: '12:50 pm',
+        instructor: 'Mr. Ramesh',
+      ),
+      ClassSchedule(
+        subjectName: 'Numbers',
+        startTime: '01:40 am',
+        endTime: '02:30 pm',
+        instructor: 'Mrs. Vani',
+      ),
+    ];
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: new SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
+        statusBarColor: Theme.of(context).scaffoldBackgroundColor,
         statusBarIconBrightness: Brightness.light,
         systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
         systemNavigationBarIconBrightness: Brightness.dark,
       ),
       child: Scaffold(
         appBar: AppBar(
-          title: Text("LearnBowl"),
-          elevation: 0,
-        brightness: Brightness.dark,
+          title: Text(
+            "LearnBowl",
+            style: TextStyle(color: Color(CustomColors.primaryMaterialColor)),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.info_outline_rounded, color: Theme.of(context).primaryColor,),
+              onPressed: () {},
+            ),
+          ],
+          elevation: 1,
+          brightness: Brightness.light,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          foregroundColor: Color(CustomColors.primaryMaterialColor),
         ),
-        body: Center(
-          child: Text("Hello World"),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 5),
+          child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            itemCount: _classSchedule.length,
+            itemBuilder: (context, index) {
+              return Schedule(_classSchedule[index]);
+            },
+          ),
         ),
       ),
     );
